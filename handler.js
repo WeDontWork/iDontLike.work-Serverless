67afa8f1d7cc1c09aacb49d4da1e85f408e4af0e
Jibin Mathews <jibinmathews7@gmail.com>
2018-04-15 02:16:03 +0530

    Response fix for api gateway

--- handler.js
+++ handler.js
@@ -2,18 +2,34 @@
 
 const lib = require('./lib');
 
+function sendResponse(error, body, event, cb) {
+  if (typeof body !== 'string') {
+    body = JSON.stringify(body);
+  }
+  const response = {
+    statusCode: error ? 400 : 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    },
+    body: JSON.stringify({ message: error || body }),
+  };
+
+  cb(null, response);
+}
+
 module.exports.createWFHReason = (event, context, callback) => {
   const body = JSON.parse(event.body);
   lib
     .handleReasonCreation(body)
     .then(result => {
       if (result.success) {
-        return callback(null, JSON.stringify({ success: true }));
+        return sendResponse(null, 'success', event, callback);
       }
-      return callback(result.error, null);
+      return sendResponse(result.error, result.error, event, callback);
     })
     .catch(err => {
-      return callback(err, null);
+      return sendResponse(err, err, event, callback);
     });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
